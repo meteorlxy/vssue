a0d89d7f888ea7ce9b95ec132709a57cd94bd34c
Xinyu Liu <meteor.lxy@foxmail.com>
2019-01-07 08:11:34 +0800

    feat($vuepress-plugin-vssue): make vssue client-only

--- packages/@vssue/vuepress-plugin-vssue/src/enhanceApp.ts
+++ packages/@vssue/vuepress-plugin-vssue/src/enhanceApp.ts
@@ -1,12 +1,35 @@
-import Vssue from 'vssue'
+import Vssue, { Vssue as VssueComponent } from 'vssue'
 import 'vssue/dist/vssue.min.css'
 // @ts-ignore
 import VssueAPI from '@vssue/api'
 
+import { VueConstructor } from 'vue'
+
 declare const VSSUE_OPTIONS: string
 
-export default ({ Vue }) => {
+declare module 'vue/types/vue' {
+  export interface VueConstructor {
+    component<Props>(id: string, definition: any): ExtendedVue<Vue, {}, {}, {}, Props>
+  }
+}
+
+export default ({ Vue }: { Vue: VueConstructor}) => {
   Vue.use(Vssue, Object.assign(JSON.parse(VSSUE_OPTIONS), {
     api: VssueAPI,
   }))
+
+  Vue.component('Vssue', {
+    functional: true,
+
+    /* eslint-disable-next-line vue/require-render-return */
+    render (h, { parent, data }) {
+      if (parent._isMounted) {
+        return h(VssueComponent, data)
+      } else {
+        parent.$once('hook:mounted', () => {
+          parent.$forceUpdate()
+        })
+      }
+    },
+  })
 }
