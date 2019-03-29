d14e9c1fd1de6a781bedaf6eff13527bdbbf9928
meteorlxy <meteor.lxy@foxmail.com>
2019-03-29 16:20:21 +0800

    chore: modify release script

--- scripts/release.js
+++ scripts/release.js
@@ -8,31 +8,30 @@ const execaOpts = {
   stdio: 'inherit',
 }
 
-const run = async (command) => {
-  const commands = command.split(' ')
+const run = async (...commands) => {
   return execa(commands[0], commands.slice(1), execaOpts)
 }
 
 const release = async () => {
   try {
-    await run(`yarn lerna version --no-git-tag-version`)
-    await run(`yarn build`)
+    await run('yarn', 'lerna', 'version', '--no-git-tag-version')
+    await run('yarn', 'build')
 
     const version = require(resolve(rootPath, 'lerna.json')).version
     const tag = `v${version}`
 
-    await run(`git add -A`)
-    await run(`git commit -m "version ${version}"`)
-    await run(`git tag ${tag} -m "version ${version}"`)
-    await run(`yarn conventional-changelog -p angular -i CHANGELOG.md -s -r 2`)
+    await run('git', 'add', '-A')
+    await run('git', 'commit', '-m', `version ${version}`)
+    await run('git', 'tag', tag, '-m', `version ${version}`)
+    await run('yarn', 'conventional-changelog', '-p', 'angular', '-i', 'CHANGELOG.md', '-s', '-r', '2')
 
     const inFile = fs.readFileSync('CHANGELOG.md').toString()
     fs.writeFileSync('CHANGELOG.md', inFile.split('\n').slice(4).join('\n'))
 
-    await run(`git add CHANGELOG.md`)
-    await run(`git commit -m "chore: version ${version} changelog"`)
-    await run(`yarn lerna publish from-package`)
-    await run(`git push origin master:master --tags`)
+    await run('git', 'add', 'CHANGELOG.md')
+    await run('git', 'commit', '-m', `chore: version ${version} changelog`)
+    await run('yarn', 'lerna', 'publish', 'from-package')
+    await run('git', 'push', 'origin', 'master:master', '--tags')
   } catch (err) {
     console.error(err)
   }
