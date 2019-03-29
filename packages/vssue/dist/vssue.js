b0e60539199b0c677f827bf797ac6a98c2576508
meteorlxy <meteor.lxy@foxmail.com>
2019-03-29 16:21:38 +0800

    version 0.8.2

--- packages/vssue/dist/vssue.js
+++ packages/vssue/dist/vssue.js
@@ -1,7 +1,7 @@
 /*!
  * vssue - A vue-powered issue-based comment plugin
  *
- * @version v0.8.1
+ * @version v0.8.2
  * @link https://vssue.js.org
  * @license MIT
  * @copyright 2018-2019 meteorlxy
@@ -1186,6 +1186,48 @@ const messages$2 = {
     deleteFailed: 'Falha ao apagar comentário',
 };
 
+const messages$3 = {
+    // auth
+    login: '{platform} でログイン',
+    logout: 'ログアウト',
+    currentUser: '現在のユーザー',
+    // comment input
+    loading: '読み込み中',
+    submit: '送信',
+    submitting: '送信中',
+    submitComment: 'コメントを送信',
+    cancel: 'キャンセル',
+    edit: '編集',
+    editMode: '編集モード',
+    delete: '削除',
+    reply: '返信',
+    // reactions
+    heart: 'ハート',
+    like: '高評価',
+    unlike: '低評価',
+    // pagination
+    perPage: 'コメント/ページ',
+    sort: '並び順を変更するにはクリックしてください',
+    page: 'ページ',
+    prev: '前のページ',
+    next: '次のページ',
+    // hint
+    comments: 'コメント | {count} コメント',
+    loginToComment: 'コメントを残すには {platform} アカウントでログインしてください。',
+    placeholder: 'コメントを残してください。Markdown 記法をサポートしています。 Ctrl + Enter で送信できます。',
+    noLoginPlaceHolder: 'コメントを残すにはログインしてください。マークダウン記法をサポートしています。',
+    // status
+    initializing: '初期化中...',
+    loadingComments: 'コメントの読み込み中...',
+    failed: 'コメントの読み込みに失敗しました',
+    requireLogin: 'コメントを見るにはログインしてください',
+    noComments: 'まだコメントがありません。最初のコメントを残しましょう！',
+    // alerts
+    reactionGiven: `既に '{reaction}' のリアクションをしています`,
+    deleteConfirm: '本当にコメントを削除してもいいですか？',
+    deleteFailed: 'コメントの削除に失敗しました',
+};
+
 if (!Vue.prototype.hasOwnProperty('$i18n')) {
     Vue.use(VueI18n);
 }
@@ -1199,6 +1241,8 @@ const i18n = new VueI18n({
         'zh-CN': messages$1,
         'pt': messages$2,
         'pt-BR': messages$2,
+        'ja': messages$3,
+        'ja-JP': messages$3,
     },
 });
 
@@ -1224,7 +1268,7 @@ let VssueStore = class VssueStore extends Vue$1 {
         this.isUpdatingComment = false;
     }
     get version() {
-        return "0.8.1";
+        return "0.8.2";
     }
     get isPending() {
         return this.isLoadingComments || this.isCreatingComment || this.isUpdatingComment;
@@ -1761,7 +1805,7 @@ var __vue_staticRenderFns__$6 = [];
 
 const VssuePlugin = {
     get version() {
-        return "0.8.1";
+        return "0.8.2";
     },
     installed: false,
     install(Vue$$1, options) {
