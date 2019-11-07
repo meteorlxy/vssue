d719a990cf393fd74a9c402b5ce50b572c29584c
meteorlxy <meteor.lxy@foxmail.com>
2019-11-07 20:50:25 +0800

    release: version 1.2.1

--- packages/vssue/dist/vssue.js
+++ packages/vssue/dist/vssue.js
@@ -1,7 +1,7 @@
 /*!
  * vssue - A vue-powered issue-based comment plugin
  *
- * @version v1.2.0
+ * @version v1.2.1
  * @link https://vssue.js.org
  * @license MIT
  * @copyright 2018-2019 meteorlxy
@@ -1299,7 +1299,7 @@ let VssueStore = class VssueStore extends Vue$1 {
         this.isUpdatingComment = false;
     }
     get version() {
-        return "1.2.0";
+        return "1.2.1";
     }
     get issueTitle() {
         if (this.options === null) {
@@ -1820,7 +1820,7 @@ var __vue_staticRenderFns__$9 = [];
 
 const VssuePlugin = {
     get version() {
-        return "1.2.0";
+        return "1.2.1";
     },
     installed: false,
     install(Vue, options) {
